ce29f39efbad1cc6bd0e57632fddad0662c72cc7
miyoung <noonchicat@gmail.com>
2021-06-28 23:13:44 +0900

    backend/ 3000 포트와 8000 포트를 연결하기 위해 django-cors-headers 설치

--- backend/backend/settings/common.py
+++ backend/backend/settings/common.py
@@ -40,11 +40,13 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
+    "corsheaders",
     "accounts",
     "delivery",
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
