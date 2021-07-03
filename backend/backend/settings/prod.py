cb88e9c4e85cce254503d8bc527aef587d007767
miyoung <noonchicat@gmail.com>
2021-07-03 21:46:51 +0900

    azure의 storage를 쓰겠다고 셋팅하고, azure storage account를 만들어 static파일을 올림.

--- backend/backend/settings/prod.py
+++ backend/backend/settings/prod.py
@@ -1,1 +1,10 @@
 from .common import *
+
+STATICFILES_STORAGE = "backend.storages.StaticAzureStorage"
+DEFAULT_FILE_STORAGE = "backend.storages.MediaAzureStorage"
+
+# AZURE_ACCOUNT_NAME = os.environ["AZURE_ACCOUNT_NAME"]
+# AZURE_ACCOUNT_KEY = os.environ["AZURE_ACCOUNT_KEY"]
+
+AZURE_ACCOUNT_NAME = "foodapp3"
+AZURE_ACCOUNT_KEY = "BKvrLyXMzLLP10tmFrSHtnVjdkb2JbjAqRF/6Y0fKnAs+RzqIBeKHJRP7U0dpk5ARiywnbhTZ559T1XA1k7LFw=="
